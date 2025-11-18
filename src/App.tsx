60893e827b889b07c2701c7c4e4e4ba93fd42d70
Ulrik-Matemu <advancechronicles@gmail.com>
2025-11-18 23:32:20 +0300

    added login page for leader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-
+import React from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import KaribuLogin  from './pages/leader/login'
 
-function App() {
+const HomePage: React.FC = () => <div>Home Page</div>
+const NotFound: React.FC = () => <div>404 - Not Found</div>
 
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <h1 className='font-extrabold'>CCP Frontend</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/leader/login" element={<KaribuLogin />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/shared/topWelcomeCard.tsx
@@ -0,0 +1,11 @@
+export const TopWelcomeCard: React.FC = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <img className="" src="/images/salim-mussa.jpeg" alt="Welcome" />
+                </div>
+            </div>
+        </>
+    )
+}
