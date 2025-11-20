eaddf89d2c5572c688c4704f87aa7e0b037ec07a
Ulrik-Matemu <advancechronicles@gmail.com>
2025-11-20 18:46:08 +0300

    preparing for production 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import KaribuLogin  from './pages/leader/login'
 import { HomeFeedPage } from './pages/home-feed-page'
 
-const HomePage: React.FC = () => <div>Home Page</div>
+
 const NotFound: React.FC = () => <div>404 - Not Found</div>
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/leader/login" element={<KaribuLogin />} />
+        <Route path="/" element={<KaribuLogin />} />
         <Route path="/home" element={<HomeFeedPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
